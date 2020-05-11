6f2325a068dd95a17aa107cb64aca3638e53427c
Dimitris <47785842+dalxds@users.noreply.github.com>
2020-05-11 21:56:34 +0300

    hp fourier

--- HP_Fourier.m
+++ HP_Fourier.m
@@ -1,5 +1,6 @@
 function HP_Fourier(ws, wp, ButterworthHighPass)
 
+global reporting;
 global plotting;
 
 Fs = 50000;           % Sampling frequency                    
@@ -7,11 +8,25 @@ T = 1/Fs;             % Sampling period
 L = 23000;            % Length of signal
 t = (0:L-1)*T;        % Time vector
 
-X =cos(0.4*ws*t) + 0.5*cos(0.9*ws*t) + cos(1.4* wp*t) + 0.7*cos(2.4*wp*t) + 0.5*cos(4.5*wp*t);
+%%% fourier frequncies
+f(1) = 0.5*ws;
+f(2) = 0.8*ws;
+f(3) = 1.2*wp;
+f(4) = 2.4*wp;
+f(5) = 3.5*wp;
+
+X = cos(f(1)*t) + 0.6*cos(f(2)*t) + cos(f(3)*t) + 0.8*cos(f(4)*t) + 0.4*cos(f(5)*t);
 Y = fft(X);
 OnlySources = abs(Y/L);
 InputFourierHighPass = OnlySources(1:L/2+1);
 InputFourierHighPass(2:end-1) = 2*InputFourierHighPass(2:end-1);
+
+if(reporting)
+    fprintf('>>> Frequencies\n');
+    for i = 1:length(f)
+        fprintf('f%i = %f Hz\n', i, f(i));
+    end
+end
 
 if(plotting)
     f = Fs*(0:(L/2))/L;
